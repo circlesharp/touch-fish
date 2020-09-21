03855184664d9bd6c94d95e30dc37406103486c0
circlesharp <circlesharp@live.com>
2020-09-21 13:38:40 +0100

    node os

---
+++ node-basic/src/22-os.js
@@ -0,0 +1,15 @@
+const os = require('os');
+
+console.log(
+  // os.arch(), // underlying architecture
+  // os.cpus(),
+  // os.freemem(),
+  // os.totalmem(),
+  // os.homedir(),
+  // os.hostname(),
+  // os.platform(),
+  // os.release(),
+  // os.tmpdir(),
+  // os.uptime(),
+  os.userInfo()
+);
