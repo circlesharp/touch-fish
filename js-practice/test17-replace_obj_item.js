987d667a70d9a66a4ba51f867853c7a73d8a5b9b
circlesharp <circlesharp@live.com>
2020-11-26 10:11:09 +0800

    11-26

---
+++ js-practice/test17-replace_obj_item.js
@@ -0,0 +1,312 @@
+const chosenRst = [
+  {
+    "BarCode": "6956416206366",
+    "Provider": "XIAOMAI",
+    "ProviderProductID": "11111",
+    "ProviderProductRawData": {
+      "avgWeight": 340,
+      "barCode": "6956416206366",
+      "createTime": 1599260564000,
+      "goodsId": 118831,
+      "imageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/MKG1592549043931.jpg",
+      "lock": 0,
+      "name": "COSTA醇正拿铁咖啡300mlPET瓶",
+      "pinyin": "123",
+      "price": 7,
+      "recognizeType": 2,
+      "sku": "瓶",
+      "status": 1
+    },
+    "CreateAt": 1599817946,
+    "UpdatedAt": 1599817946,
+    "id": "5f5b48da5b8c12000fe2a423",
+    "ProviderImageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/MKG1592549043931.jpg"
+  },
+  {
+    "BarCode": "6925303757472",
+    "Provider": "XIAOMAI",
+    "ProviderProductID": "22222",
+    "ProviderProductRawData": {
+      "avgWeight": 500,
+      "createTime": 1575093383000,
+      "desc": "海之言（百香果）",
+      "goodsId": 16241,
+      "imageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/MKG1565752829611.com/goods/1557474673266_26421",
+      "name": "海之言（百香果）",
+      "pinyin": "hzy（bxg）",
+      "price": 5,
+      "recognizeType": 1,
+      "sku": "",
+      "status": 1
+    },
+    "CreateAt": 1585038481,
+    "UpdatedAt": 1585038481,
+    "id": "5e79c49185536800105258e1",
+    "ProviderImageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/MKG1565752829611.com/goods/1557474673266_26421"
+  },
+  {
+    "BarCode": "6971583190010",
+    "Provider": "XIAOMAI",
+    "ProviderProductID": "119348",
+    "ProviderProductRawData": {
+      "avgWeight": 300,
+      "barCode": "6971583190010",
+      "createTime": 1593767328000,
+      "goodsId": 119348,
+      "imageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/35a4c2891e616035bd31b9e49c925c8a_1593765724048.jpg",
+      "name": "亚牛罐装250ml",
+      "pinyin": "123",
+      "price": 3,
+      "recognizeType": 1,
+      "sku": "250",
+      "status": 1
+    },
+    "CreateAt": 1593767329,
+    "UpdatedAt": 1593767329,
+    "id": "5efef5a1739e5c000f463773",
+    "ProviderImageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/35a4c2891e616035bd31b9e49c925c8a_1593765724048.jpg"
+  },
+  {
+    "BarCode": "0202006101660",
+    "Provider": "XIAOMAI",
+    "ProviderProductID": "118482",
+    "ProviderProductRawData": {
+      "avgWeight": 1,
+      "barCode": "",
+      "createTime": 1591771411000,
+      "goodsId": 118482,
+      "imageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/64d1c1ac454d381260ea72fa4b917dbe_1591770247239.jpg",
+      "name": "宅优鲜半成品菜8",
+      "pinyin": "123",
+      "price": 0,
+      "recognizeType": 1,
+      "sku": "",
+      "status": 1
+    },
+    "CreateAt": 1591785006,
+    "UpdatedAt": 1591785006,
+    "id": "5ee0b62eeb9f07000f4f8f9e",
+    "ProviderImageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/64d1c1ac454d381260ea72fa4b917dbe_1591770247239.jpg"
+  },
+  {
+    "BarCode": "6901939371565",
+    "Provider": "XIAOMAI",
+    "ProviderProductID": "119356",
+    "ProviderProductRawData": {
+      "avgWeight": 380,
+      "barCode": "6901939371565",
+      "createTime": 1593768621000,
+      "goodsId": 119356,
+      "imageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/d8bbb487fc4a2d8f1b499491988650ba_1593766919620.jpg",
+      "name": "冰露350mlPET瓶",
+      "pinyin": "123",
+      "price": 3,
+      "recognizeType": 1,
+      "sku": "350",
+      "status": 1
+    },
+    "CreateAt": 1593768620,
+    "UpdatedAt": 1593768620,
+    "id": "5efefaac739e5c000f46377b",
+    "ProviderImageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/d8bbb487fc4a2d8f1b499491988650ba_1593766919620.jpg"
+  },
+  {
+    "BarCode": "6902538004045",
+    "Provider": "XIAOMAI",
+    "ProviderProductID": "17006",
+    "ProviderProductRawData": {
+      "avgWeight": 650,
+      "createTime": 1578988094000,
+      "desc": "",
+      "goodsId": 17006,
+      "imageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/MKG1565753025717.com/goods/1561811637526_37169",
+      "name": "脉动青柠",
+      "pinyin": "mdqn",
+      "price": 6.5,
+      "recognizeType": 1,
+      "sku": "1升",
+      "status": 1
+    },
+    "CreateAt": 1585034828,
+    "UpdatedAt": 1585724715,
+    "id": "5e79b64c85536800105258d2",
+    "ProviderImageUrl": "http://make-goods.oss-cn-hangzhou.aliyuncs.com/MKG1565753025717.com/goods/1561811637526_37169"
+  }
+];
+
+const CargoConfig = [
+  {
+    "Row": 1,
+    "Col": 1,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 1,
+    "Col": 2,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 1,
+    "Col": 3,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 1,
+    "Col": 4,
+    "BarCode": "6925303757472",
+    "ProviderProductID": "16241",
+    "ProductVO": {
+      "Name": "海之言百香果口味瓶装500ml",
+      "ImageUrl": "https://res.yopoint.com/product/20190612/41ea8e7bd5689da941b5c80babc8f48c.png",
+      "ImageFixWidthUrl": "https://res.yopoint.com/product/20190612/2cf1b1e248d414b9b47069e7324fd2b5.png"
+    }
+  },
+  {
+    "Row": 1,
+    "Col": 5,
+    "BarCode": "6956416206366",
+    "ProviderProductID": "118831",
+    "ProductVO": {
+      "Name": "COSTA COFFEE醇正低脂肪醇正拿铁浓咖啡300mlPET瓶",
+      "ImageUrl": "http://dev-res.yopoint.cc/product/20200911/c1a6af541d3695f30ccddd835ae91661.png",
+      "ImageFixWidthUrl": "http://dev-res.yopoint.cc/product/20200911/f7aa2404e001a2889f24568b32073b60.png"
+    }
+  },
+  {
+    "Row": 1,
+    "Col": 6,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 2,
+    "Col": 1,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 2,
+    "Col": 2,
+    "BarCode": "6954767430461",
+    "ProviderProductID": "11639",
+    "ProductVO": {
+      "Name": "雪碧纤维+500mlPET瓶",
+      "ImageUrl": "https://res.yopoint.com/product/20180328/a8c66eeb3bdf6080aa7d28b6ec06b687.png",
+      "ImageFixWidthUrl": "https://res.yopoint.com/product/20180328/09c344e99bae1b7c9ac78359f130edbb.png"
+    }
+  },
+  {
+    "Row": 2,
+    "Col": 3,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 2,
+    "Col": 4,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 2,
+    "Col": 5,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 2,
+    "Col": 6,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 3,
+    "Col": 1,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 3,
+    "Col": 2,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 3,
+    "Col": 3,
+    "BarCode": "6971583190010",
+    "ProviderProductID": "119348",
+    "ProductVO": {
+      "Name": "亚牛罐装250ml",
+      "ImageUrl": "http://dev-res.yopoint.cc/operators/20200703/35a4c2891e616035bd31b9e49c925c8a.jpg",
+      "ImageFixWidthUrl": "http://dev-res.yopoint.cc/operators/20200703/35a4c2891e616035bd31b9e49c925c8a.jpg"
+    }
+  },
+  {
+    "Row": 3,
+    "Col": 4,
+    "BarCode": "0202006101660",
+    "ProviderProductID": "118482",
+    "ProductVO": {
+      "Name": "宅优鲜半成品菜8",
+      "ImageUrl": "http://dev-res.yopoint.cc/operators/20200609/64d1c1ac454d381260ea72fa4b917dbe.jpg",
+      "ImageFixWidthUrl": "http://dev-res.yopoint.cc/operators/20200609/64d1c1ac454d381260ea72fa4b917dbe.jpg"
+    }
+  },
+  {
+    "Row": 3,
+    "Col": 5,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 3,
+    "Col": 6,
+    "BarCode": "6901939371565",
+    "ProviderProductID": "119356",
+    "ProductVO": {
+      "Name": "冰露饮用水350mlPET瓶",
+      "ImageUrl": "https://res.yopoint.com/product/20190129/057e9c22db7083a4a54b2e7b9c341b89.png",
+      "ImageFixWidthUrl": "https://res.yopoint.com/product/20190129/8f5c2015590d704495324e6307f4dc42.png"
+    }
+  },
+  {
+    "Row": 4,
+    "Col": 1,
+    "BarCode": "6902538004045",
+    "ProviderProductID": "17006",
+    "ProductVO": {
+      "Name": "脉动青柠口味瓶装600ml",
+      "ImageUrl": "https://res.yopoint.com/product/20190506/48dea89ca7e1fab3d5d370621a0a7e2b.png",
+      "ImageFixWidthUrl": "https://res.yopoint.com/product/20190506/8b876b7b4025f2b25698e3d4638d513a.png"
+    }
+  },
+  {
+    "Row": 5,
+    "Col": 1,
+    "BarCode": "",
+    "ProviderProductID": ""
+  },
+  {
+    "Row": 6,
+    "Col": 1,
+    "BarCode": "",
+    "ProviderProductID": ""
+  }
+];
+
+const replaceProviderProductID = (chosenRst, CargoConfig) => {
+  chosenRst.forEach(rst => {
+    CargoConfig.forEach(config => {
+      if (rst.BarCode === config.BarCode)
+        config.ProviderProductID = rst.ProviderProductID;
+    })
+  })
+}
+
+replaceProviderProductID(chosenRst, CargoConfig);
+
+console.log(JSON.stringify(CargoConfig, null, 2));
