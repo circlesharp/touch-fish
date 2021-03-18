e16723760fbbafa31be6276fcc001420a094f436
circlesharp <circlesharp@live.com>
2021-03-18 10:36:14 +0800

    使用正则改写 Money Class

--- js-practice/test45_money-class-2.js
+++ js-practice/test45_money-class-2.js
@@ -1,11 +1,11 @@
 class Money {
-  constructor(value, isCent) {
+  constructor(value, isCent = false) {
     if (isCent) {
       this.value = +value;
       this.label = this._cent2label(this.value);
     } else {
-      this.label = value + '';
-      this.value = this._label2cent(this.label);
+      this.value = this._label2cent(value + '');
+      this.label = this._cent2label(this.value);
     }
   }
 
@@ -15,32 +15,35 @@ class Money {
   }
 
   _cent2label(cent) {
-    let moneyStr = (cent / 100) + '';
+    const moneyStr = (cent / 100) + '';
 
-    if (moneyStr.indexOf('.') === -1) {
-      moneyStr = `${moneyStr}.00`;
-    } else if (moneyStr.indexOf('.') === moneyStr.length - 2) {
-      moneyStr = `${moneyStr}0`;
-    }
-
-    return `${moneyStr}`;
+    if (/^(?!.*\.)/.test(moneyStr))
+      // 不含小数点
+      return `${moneyStr}.00`;
+    else if (/\..{1}$/.test(moneyStr))
+      // 仅有小数点后一位
+      return `${moneyStr}0`;
+    else
+      return moneyStr;
   }
 }
 
+const testHelper = i => {
+  const { label, value } = new Money(i);
+  console.log(i, label, value);
+};
+
 // test 1 元，整数
 const testCollection = [0, 1, 2, 100, 120, 1234, -1, -2, -100, -1234, -123456];
-testCollection.forEach(i => {
-  // console.log(new Money(i))
-});
+testCollection.forEach(testHelper);
 
 // test 2 分，整数
 const testCollectionCent = [0, 1, 2, 100, 120, 1234, -1, -2, -100, -1234, -123456];
 testCollectionCent.forEach(i => {
-  // console.log(new Money(i, true))
+  const { label, value } = new Money(i, true);
+  console.log(i, label, value);
 });
 
 // test 3 元，小数
-const testCollection2 = [1.11, 1111.11, 22.00, 22.22];
-testCollection2.forEach(i => {
-  // console.log(new Money(i))
-});
+const testCollection2 = [1.11, 1111.11, 22.00, 22.22, 0.01, 0.009, 0.001];
+// testCollection2.forEach(testHelper);
