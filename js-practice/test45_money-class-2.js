2966c0fab26f6f659e06f9f44947a9a2f72abcf0
circlesharp <circlesharp@live.com>
2021-03-18 10:55:55 +0800

    使用正则改写 Money Class
    1. 优化保留小数点后两位的逻辑
    2. 处理千分位&万分位

--- js-practice/test45_money-class-2.js
+++ js-practice/test45_money-class-2.js
@@ -1,11 +1,11 @@
 class Money {
-  constructor(value, isCent = false) {
+  constructor(value, isCent = false, divide) {
     if (isCent) {
-      this.value = +value;
-      this.label = this._cent2label(this.value);
+      this.value = Math.round(+value);
+      this.label = this._cent2label(this.value, divide);
     } else {
       this.value = this._label2cent(value + '');
-      this.label = this._cent2label(this.value);
+      this.label = this._cent2label(this.value, divide);
     }
   }
 
@@ -14,17 +14,26 @@ class Money {
     return value;
   }
 
-  _cent2label(cent) {
-    const moneyStr = (cent / 100) + '';
+  _cent2label(cent, divide) {
+    let moneyStr = (cent / 100) + '';
 
+    // 补齐小数点后两位
     if (/^(?!.*\.)/.test(moneyStr))
       // 不含小数点
-      return `${moneyStr}.00`;
-    else if (/\..{1}$/.test(moneyStr))
+      moneyStr =  `${moneyStr}.00`;
+    if (/\..{1}$/.test(moneyStr))
       // 仅有小数点后一位
-      return `${moneyStr}0`;
-    else
-      return moneyStr;
+      moneyStr = `${moneyStr}0`;
+
+
+    // 处理 千分位 / 万分位
+    if (divide != null) {
+      const regexp = new RegExp(`[1-9]\\d{0,${divide - 1}}(?=(\\d{${divide}})+$)`, 'g');
+      const [integer, decimal] = moneyStr.split('.');
+      moneyStr = [integer.replace(regexp, '$&,'), decimal].join('.');
+    }
+
+    return moneyStr;
   }
 }
 
@@ -35,15 +44,22 @@ const testHelper = i => {
 
 // test 1 元，整数
 const testCollection = [0, 1, 2, 100, 120, 1234, -1, -2, -100, -1234, -123456];
-testCollection.forEach(testHelper);
+// testCollection.forEach(testHelper);
 
 // test 2 分，整数
 const testCollectionCent = [0, 1, 2, 100, 120, 1234, -1, -2, -100, -1234, -123456];
-testCollectionCent.forEach(i => {
-  const { label, value } = new Money(i, true);
-  console.log(i, label, value);
-});
+// testCollectionCent.forEach(i => {
+//   const { label, value } = new Money(i, true);
+//   console.log(i, label, value);
+// });
 
 // test 3 元，小数
 const testCollection2 = [1.11, 1111.11, 22.00, 22.22, 0.01, 0.009, 0.001];
 // testCollection2.forEach(testHelper);
+
+// test 4 千分位
+const testCollection3 = [11111.11, 22, 22222222222, 0.001, 123456789.125, 41876.44442222221];
+testCollection3.forEach(i => {
+  const { label, value } = new Money(i, true, 3);
+  console.log(i, label, value);
+});
