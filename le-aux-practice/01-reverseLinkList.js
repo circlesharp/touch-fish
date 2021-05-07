e2f8641eb08a1d6029196fcfcdafbb691309f26a
circlesharp <circlesharp@live.com>
2021-05-07 17:56:53 +0800

    feat(le-aux): 递归翻转链表前N项 & 翻转链表的一部分

--- le-aux-practice/01-reverseLinkList.js
+++ le-aux-practice/01-reverseLinkList.js
@@ -10,4 +10,36 @@ const reverse = head => {
   return last;
 }
 
-module.exports = { reverse };
+const reverseN = (head, n) => {
+  let afterNNode, last;
+  const _reverseN = (head, n) => {
+    if (n <= 1) {
+      afterNNode = head.next;
+      return head;
+    }
+
+    last = _reverseN(head.next, n - 1);
+    head.next.next = head;
+    head.next = afterNNode;
+
+    return last;
+  }
+
+  return _reverseN(head, n);
+}
+
+const reverseBetween = (head, m, n) => {
+  if (m === 1) {
+    return reverseN(head, n);
+  }
+
+  head.next = reverseBetween(head.next, m - 1, n - 1);
+
+  return head;
+}
+
+module.exports = {
+  reverse,
+  reverseN,
+  reverseBetween,
+};
--- le-aux-practice/test/01.spec.js
+++ le-aux-practice/test/01.spec.js
@@ -1,5 +1,5 @@
 const { getLinkList, arraifyLinkList } = require('../../utils/LinkList');
-const { reverse } = require('../01-reverseLinkList');
+const { reverse, reverseN, reverseBetween } = require('../01-reverseLinkList');
 
 describe('翻转链表', () => {
   it('递归翻转整个链表', () => {
@@ -8,5 +8,21 @@ describe('翻转链表', () => {
     const revLinkList = reverse(linkList);
     expect(arraifyLinkList(revLinkList))
       .toEqual(Array.from(arr).reverse());
+  });
+
+  it('递归翻转链表前N项', () => {
+    const arr = [1, 2, 3, 4]
+    const linkList = getLinkList(arr);
+    const revLinkList = reverseN(linkList, 3);
+    expect(arraifyLinkList(revLinkList))
+      .toEqual([3, 2, 1, 4]);
+  });
+
+  it('翻转链表的一部分', () => {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const linkList = getLinkList(arr);
+    const revLinkList = reverseBetween(linkList, 2, 5);
+    expect(arraifyLinkList(revLinkList))
+      .toEqual([1, 5, 4, 3, 2, 6]);
   });
 });
