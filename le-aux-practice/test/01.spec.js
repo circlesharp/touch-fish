3e619b2b89ed3d70a7469c2b4ed7918a8fa8aacf
circlesharp <circlesharp@live.com>
2021-05-07 17:10:17 +0800

    test(config): 优化测试代码

--- le-aux-practice/test/01.spec.js
+++ le-aux-practice/test/01.spec.js
@@ -1,10 +1,12 @@
 const { getLinkList, arraifyLinkList } = require('../../utils/LinkList');
-const { reverse } = require('../01-递归翻转链表');
+const { reverse } = require('../01-reverseLinkList');
 
-it('递归翻转链表', () => {
-  const arr = [1, 2, 3, 4]
-  const linkList = getLinkList(arr);
-  const revLinkList = reverse(linkList);
-  expect(arraifyLinkList(revLinkList))
-    .toEqual(Array.from(arr).reverse());
+describe('翻转链表', () => {
+  it('递归翻转整个链表', () => {
+    const arr = [1, 2, 3, 4]
+    const linkList = getLinkList(arr);
+    const revLinkList = reverse(linkList);
+    expect(arraifyLinkList(revLinkList))
+      .toEqual(Array.from(arr).reverse());
+  });
 });
--- node-basic/test/test.js
+++
@@ -1,6 +0,0 @@
-let killItself = () => {
-  console.log('start to kill itself...');
-  killItself = null;
-};
-killItself();
-console.log(`After killing, the killItself is: ${killItself}.`);
