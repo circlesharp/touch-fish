e1ba4fe165333215e80eacd4ecb70e14e09240a9
circlesharp <circlesharp@live.com>
2021-05-13 20:49:54 +0800

    feat(le-aux): 判断回文链表（空间复杂度 O(1)）

--- le-aux-practice/03-isPalindrome.js
+++ le-aux-practice/03-isPalindrome.js
@@ -1,3 +1,5 @@
+const { reverse } = require('./01-reverseLinkList');
+
 /* 链表前序遍历 */
 const preTraversal = (head, visit = console.log) => {
   if (!head) {
@@ -16,8 +18,9 @@ const postTraversal = (head, visit = console.log) => {
 
   postTraversal(head.next, visit);
   visit(head);
-}
+};
 
+/* 判断回文链表（空间复杂度 O(n) */
 const isPalindrome = head => {
   let left = head;
 
@@ -31,13 +34,43 @@ const isPalindrome = head => {
     left = left.next;
 
     return res;
-  }
+  };
 
   return _traversal(head);
-}
+};
+
+/* 判断回文链表（空间复杂度 O(1) */
+const isPalindrome2 = head => {
+  /* 通过快慢指针来找到链表的中点 */
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  if (fast) {
+    /* 基数长度，slow 仍需后退一位 */
+    slow = slow.next;
+  }
+
+  /* 从 slow 开始反转后面的链表 */
+  let left = head;
+  let right = reverse(slow);
+  while (right) {
+    if (left.data !== right.data) {
+      return false;
+    }
+
+    left = left.next;
+    right = right.next;
+  }
+
+  return true;
+};
 
 module.exports = {
   preTraversal,
   postTraversal,
   isPalindrome,
+  isPalindrome2,
 };
--- le-aux-practice/test/03.spec.js
+++ le-aux-practice/test/03.spec.js
@@ -1,12 +1,12 @@
-const { getLinkList, arraifyLinkList } = require('../../utils/LinkList');
-const { preTraversal, postTraversal, isPalindrome } = require('../03-isPalindrome');
+const { getLinkList } = require('../../utils/LinkList');
+const { preTraversal, postTraversal, isPalindrome, isPalindrome2 } = require('../03-isPalindrome');
 
 describe('判断回文链表', () => {
   it('链表前序遍历', () => {
     const arr = [1, 2, 3, 4];
     const rst = [];
     const linkList = getLinkList(arr);
-    preTraversal(linkList, node => { rst.push(node.data) })
+    preTraversal(linkList, node => { rst.push(node.data); });
 
     expect(rst)
       .toEqual(arr);
@@ -16,7 +16,7 @@ describe('判断回文链表', () => {
     const arr = [1, 2, 3, 4];
     const rst = [];
     const linkList = getLinkList(arr);
-    postTraversal(linkList, node => { rst.push(node.data) })
+    postTraversal(linkList, node => { rst.push(node.data); });
 
     expect(rst)
       .toEqual(arr.reverse());
@@ -35,5 +35,19 @@ describe('判断回文链表', () => {
     expect(isPalindrome(ll2)).toEqual(true);
     expect(isPalindrome(ll3)).toEqual(false);
   });
-})
+
+  it('判断回文链表（空间复杂度 O(1)）', () => {
+    const arr1 = [1, 2, 3, 4, 5, 4, 3, 2, 1];
+    const arr2 = [1, 2, 3, 4, 4, 3, 2, 1];
+    const arr3 = [1, 2, 3, 4, 5, 3, 2, 1];
+
+    const ll1 = getLinkList(arr1);
+    const ll2 = getLinkList(arr2);
+    const ll3 = getLinkList(arr3);
+
+    expect(isPalindrome2(ll1)).toEqual(true);
+    expect(isPalindrome2(ll2)).toEqual(true);
+    expect(isPalindrome2(ll3)).toEqual(false);
+  });
+});
 
