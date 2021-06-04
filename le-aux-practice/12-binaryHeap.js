f8d85712dee2ebb22592fe55602d933872de7e5e
circlesharp <circlesharp@live.com>
2021-06-04 10:16:00 +0800

    feat(le-aux): binary heap && priority queue

---
+++ le-aux-practice/12-binaryHeap.js
@@ -0,0 +1,100 @@
+/* Binary Heap & Priority Queue */
+/* 使用二叉堆实现优先队列 */
+/* 二叉堆是二叉树，是完全二叉树，（最大堆的）每个结点都大于等于它的子结点 */
+/* 通过 swim, sink 维护二叉堆的性质 */
+
+class PriorityQueue {
+  constructor(maxSize = 100) {
+    /* 第 0 位不使用 */
+    this._pq = Array(maxSize + 1);
+    this._len = 0;
+  }
+
+  getMax() {
+    return this._pq[1];
+  }
+
+  insert(val) {
+    this._len += 1;
+    this._pq[this._len] = val;
+    this._swim(this._len);
+  }
+
+  deleteMax() {
+    const max = this.getMax();
+    this._exch(1, this._len);
+    this._pq[this._len] = undefined;
+    this._len -= 1;
+    this._sink(1);
+
+    return max;
+  }
+
+  /* 上浮第 k 个元素 */
+  _swim(k) {
+    const parentIdx = PriorityQueue._getParentIdx(k);
+    if (k <= 1 || this._less(k, parentIdx)) {
+      return;
+    }
+
+    this._exch(k, parentIdx);
+    this._swim(parentIdx);
+  }
+
+  /* 下沉第 k 个元素 */
+  _sink(k) {
+    const leftIdx = PriorityQueue._getLeftIdx(k);
+    const rightIdx = PriorityQueue._getRightIdx(k);
+
+    /* 边界条件1 如果没有子结点，就在堆底 */
+    if (leftIdx > this._len) {
+      return;
+    }
+
+    /* 边界条件2 父结点不比较大的子结点小 */
+    let largerChildIdx;
+    if (rightIdx <= this._len && this._less(leftIdx, rightIdx)) {
+      largerChildIdx = rightIdx;
+    } else {
+      largerChildIdx = leftIdx;
+    }
+    if (!this._less(k, largerChildIdx)) {
+      return;
+    }
+
+    this._exch(k, largerChildIdx);
+    this._sink(largerChildIdx);
+  }
+
+  /* 交换 i, j 元素 */
+  _exch(i, j) {
+    const temp = this._pq[i];
+    this._pq[i] = this._pq[j];
+    this._pq[j] = temp;
+  }
+
+  /* 判断第 i 个元素是否比第 j 个小 */
+  _less(i, j) {
+    return this._pq[i] < this._pq[j];
+  }
+
+  _getPQ() {
+    return this._pq.slice(1, this._len + 1);
+  }
+
+  static _getParentIdx(k) {
+    return k / 2 | 0;
+  }
+
+  static _getLeftIdx(k) {
+    return k * 2;
+  }
+
+  static _getRightIdx(k) {
+    return k * 2 + 1;
+  }
+}
+
+module.exports = {
+  PriorityQueue,
+}
---
+++ le-aux-practice/test/12.spec.js
@@ -0,0 +1,59 @@
+const { PriorityQueue } = require('../12-binaryHeap');
+
+describe('PriorityQueue', () => {
+  it('insert', () => {
+    const priorityQueue = new PriorityQueue();
+    const arr = [1, 2, 3, 4, 5, 6, 7];
+    const rst = [
+      [1],
+      [2, 1],
+      [3, 1, 2],
+      [4, 3, 2, 1],
+      [5, 4, 2, 1, 3],
+      [6, 4, 5, 1, 3, 2],
+      [7, 4, 6, 1, 3, 2, 5],
+    ];
+    for (let i = 0; i < arr.length; i++) {
+      priorityQueue.insert(arr[i]);
+      expect(priorityQueue._getPQ()).toEqual(rst[i]);
+      expect(priorityQueue.getMax()).toEqual(arr[i]);
+    }
+  });
+
+  it('delete max', () => {
+    const priorityQueue = new PriorityQueue();
+    const arr = [1, 2, 3, 4, 5, 6, 7];
+    const rst = [
+      [6, 4, 5, 1, 3, 2],
+      [5, 4, 2, 1, 3],
+      [4, 3, 2, 1],
+      [3, 1, 2],
+      [2, 1],
+      [1],
+      [],
+    ];
+    for (let i = 0; i < arr.length; i++) {
+      priorityQueue.insert(arr[i]);
+    }
+    for (let i = 0; i < arr.length; i++) {
+      expect(priorityQueue.deleteMax()).toEqual(arr[arr.length - 1 - i]);
+      expect(priorityQueue._getPQ()).toEqual(rst[i]);
+    }
+  });
+
+  it('random test', () => {
+    const priorityQueue = new PriorityQueue();
+    const arr = [11, 2, 555, 3, 6646, 9, 1, 23, 56, 9090, 9090, 3];
+    const rst = Array.from(arr).sort((a, b) => b - a);
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      priorityQueue.insert(arr[i]);
+    }
+    for (let i = 0; i < arr.length; i++) {
+      result.push(priorityQueue.deleteMax());
+    }
+
+    expect(result).toEqual(rst);
+  })
+});
