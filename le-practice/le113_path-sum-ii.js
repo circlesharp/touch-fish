1dd56099747b4be0650b0a44cf231c152d744ee7
circlesharp <circlesharp@live.com>
2021-06-25 17:03:40 +0800

    feat(le): 113

---
+++ le-practice/le113_path-sum-ii.js
@@ -0,0 +1,28 @@
+const solution = (root, target) => {
+  const result = [];
+  const path = [];
+  _traversal(root, target, result, path);
+
+  return result;
+}
+
+const _traversal = (root, target, result, path) => {
+  /* 处理根节点 */
+  if (!root) {
+    return;
+  }
+
+  path.push(root.val);
+
+  /* 处理叶节点 */
+  if (!root.left && !root.right) {
+    if (target === root.val) {
+      result.push(Array.from(path));
+    }
+  }
+
+  _traversal(root.left, target - root.val, result, path)
+  _traversal(root.right, target - root.val, result, path);
+
+  path.pop();
+}
