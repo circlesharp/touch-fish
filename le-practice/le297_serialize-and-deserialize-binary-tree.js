3f638499ff38acb3be235d86dd958bb6aedadf95
circlesharp <circlesharp@live.com>
2021-07-12 18:18:07 +0800

    feat(le): 297 前序遍历

---
+++ le-practice/le297_serialize-and-deserialize-binary-tree.js
@@ -0,0 +1,38 @@
+const NULL = null;
+
+const serialize = root => {
+  const result = [];
+
+  if (!root) return result;
+
+  _traversal(root, result);
+
+  return result;
+};
+
+const _traversal = (root, result) => {
+  if (!root) {
+    result.push(NULL);
+    return;
+  }
+
+  result.push(root.val);
+
+  _traversal(root.left, result);
+  _traversal(root.right, result);
+}
+
+const deserialize = arr => {
+  if (arr.length === 0) return null;
+
+  const val = arr.shift();
+  if (val === NULL) {
+    return null;
+  } else {
+    const root = val === NULL ? null : new TreeNode(val);
+    root.left = deserialize(arr);
+    root.right = deserialize(arr);
+
+    return root;
+  }
+}
