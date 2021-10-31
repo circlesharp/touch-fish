b36cd95e2133d3473f1473608429d7abe59aa54b
circlesharp <circlesharp@live.com>
2021-10-31 09:20:01 +0800

    feat(vue-ssr): Code-Splitting

--- vue-ssr/src/routing-and-code-splitting/router.js
+++ vue-ssr/src/routing-and-code-splitting/router.js
@@ -3,9 +3,21 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 
+// loading just what is needed.
+const routes = [
+  {
+    path: '/',
+    component: () => import('./components/Home.vue'),
+  },
+  {
+    path: '/item/:id',
+    component: () => import('./components/Item.vue'),
+  },
+];
+
 export function createRouter() {
   return new Router({
     mode: 'history',
-    routes: [],
+    routes,
   });
 }
