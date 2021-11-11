034bbac7d63f1def16902411692f5a95b59526b1
circlesharp <circlesharp@live.com>
2021-11-12 07:58:51 +0800

    feat(pack): webpack official 防止重复-1 entry dependOn

--- pack/src/08_official-course/src/index.js
+++ pack/src/08_official-course/src/index.js
@@ -1,15 +1,8 @@
 import _ from 'lodash';
-import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
-
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.appendChild(btn);
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
 
   return element;
 }
--- pack/src/08_official-course/webpack.config.js
+++ pack/src/08_official-course/webpack.config.js
@@ -4,8 +4,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: path.resolve(__dirname, './src/index.js'),
-    print: path.resolve(__dirname, './src/print.js'),
+    index: {
+      import: path.resolve(__dirname, './src/index.js'),
+      dependOn: 'shared',
+    },
+    another: {
+      import: path.resolve(__dirname, './src/another-module.js'),
+      dependOn: 'shared',
+    },
+    shared: 'lodash',
   },
   output: {
     filename: '[name].bundle.js',
@@ -23,5 +30,8 @@ module.exports = {
     // http://[devServer.host]:[devServer.port]/[output.publicPath]/[output.filename]
     // 将 dist 目录下的文件 serve 到 localhost:8080 下, 默认就是 output.path
     static: path.resolve(__dirname, '../../dist/build08'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
