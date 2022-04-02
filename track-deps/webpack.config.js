4fac778685d9dd78e9c77cb4e924c05bfb82132c
circlesharp <circlesharp@live.com>
2022-04-02 20:19:34 +0800

    feat(webpack): 通过显式配置 providedExports, usedExports 保证获取依赖的结果

--- track-deps/webpack.config.js
+++ track-deps/webpack.config.js
@@ -10,6 +10,15 @@ module.exports = {
     filename: '[name].chunk.js',
   },
   plugins: [new TemplatePlugin('src')],
+  optimization: {
+    // 默认 true, 告知 webpack 去确定那些由模块提供的导出内容
+    // 设置为 false 之后会导致取不到 _lastSuccessfulBuildMeta
+    providedExports: true,
+
+    // 告知 webpack 去决定每个模块使用的导出内容
+    // 设置为 false 之后会导致取不到 usedExports
+    usedExports: true,
+  },
   module: {
     rules: [
       {
