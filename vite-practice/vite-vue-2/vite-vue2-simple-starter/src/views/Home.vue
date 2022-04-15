22897b2abec73a5bfa2dfa22402d501c35431a98
circlesharp <circlesharp@live.com>
2022-04-15 22:18:35 +0800

    feat(vite): 增加 vite 练习

---
+++ vite-practice/vite-vue-2/vite-vue2-simple-starter/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>THIS IS HOME</div>
+</template>
+<script>
+import HomeApi from "@/api/modules/HomeApi";
+export default {
+  name: "Home",
+  components: {},
+  computed: {},
+  data: () => ({}),
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  activated() {}, //when keep-alive
+  deactivated() {}, //when keep-alive
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {
+    // getDatList(){
+    //   HomeApi.getDatList()
+    // }
+  },
+};
+</script>
---
+++ vite-practice/vite-vue-2/vite-vue2-simple-starter/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin } from 'vite-plugin-vue2';
+import path from 'path';
+
+const HOST = '0.0.0.0';
+const REPLACEMENT = `${path.resolve(__dirname, './src')}/`;
+
+export default (/** if you want to use mode : { mode }*/) => {
+  return defineConfig({
+    base: './',
+    server: {
+      host: HOST,
+      port: process.env.PORT,
+    },
+    resolve: {
+      alias: [
+        {
+          find: '@/',
+          replacement: REPLACEMENT,
+        },
+        {
+          find: 'src/',
+          replacement: REPLACEMENT,
+        },
+      ],
+    },
+    plugins: [createVuePlugin(/* options */)],
+  });
+};
