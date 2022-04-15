091d78654c8aa77e4735a47151262550699022ed
circlesharp <circlesharp@live.com>
2022-04-16 01:29:56 +0800

    feat(vite): 使用 vite dev vue 项目

---
+++ webpack-practice/src/07_vue/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin } from 'vite-plugin-vue2';
+
+const HOST = '0.0.0.0';
+
+export default (/** if you want to use mode : { mode }*/) => {
+  return defineConfig({
+    root: __dirname,
+    base: './',
+    server: {
+      host: HOST,
+      port: process.env.PORT,
+    },
+    plugins: [createVuePlugin(/* options */)],
+  });
+};
