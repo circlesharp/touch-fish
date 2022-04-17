121e9a7e2ceb7949cbc82b9d7d988f252971d8ad
circlesharp <circlesharp@live.com>
2022-04-17 11:52:14 +0800

    feat(rollup): 使用 rollup 打包

---
+++ track-deps/rollup/rollup.config.js
@@ -0,0 +1,18 @@
+import path from 'path';
+import url from 'url';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: path.resolve(__dirname, './dist/index.es.js'),
+      format: 'es',
+    },
+    {
+      file: path.resolve(__dirname, './dist/index.cjs.js'),
+      format: 'cjs',
+    },
+  ],
+};
--- track-deps/src/index.js
+++ track-deps/src/index.js
@@ -1,5 +1,5 @@
 import a from './components/a';
 import { aaa, bbb } from './components/a';
-import './style/style.css';
+// import './style/style.css';
 
 console.log(a, aaa, bbb);
